d3065c6ffa927e51ccee2d122d43941020a9f0f9
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 15:54:43 +0800

    pseudocode

--- remove-first-n-characters-from-a-string.py
+++ remove-first-n-characters-from-a-string.py
@@ -1,1 +1,7 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# Pseudocode
+
+# code to remove characters from a string starting from index n
+# Check if n is within the limit of the word. if not, provide a error message
+# remove char function and print the result
