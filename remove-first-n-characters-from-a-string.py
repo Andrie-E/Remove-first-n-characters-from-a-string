259cdb7559c70c42dc54eed19ddc04da7e7c729c
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 15:59:15 +0800

    new pseudocode and code for if-else statement

--- remove-first-n-characters-from-a-string.py
+++ remove-first-n-characters-from-a-string.py
@@ -3,5 +3,10 @@
 # Pseudocode
 
 # code to remove characters from a string starting from index n
+def remove_char(input_string, n):
 # Check if n is within the limit of the word. if not, provide a error message
+    return input_string[n:] if 0 <= n < len(input_string) else "Provide accurate information please!"
+# input user code
+user_input = input("please provide a word: ")
+n = int(input("what is the value of n: "))
 # remove char function and print the result
